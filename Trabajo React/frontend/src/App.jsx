69aecbe8689257590a601bcc4e498d997c9c3ffa
Matias Bruzzo <matiaslbruzzo@gmail.com>
2024-06-12 12:44:29 -0300

    todo

--- Trabajo React/frontend/src/App.jsx
+++ Trabajo React/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Synthwave from "./pages/Synthwave";
 import Vaporwave from "./pages/Vaporwave";
 import Peliculas from "./pages/Peliculas";
+import Musica from "./pages/Musica";
 
 function App() {
   return (
---
+++ Trabajo React/frontend/src/pages/Musica.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Musica = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <header>
+          <div className="holder header">
+            <img
+              src="./imagenes/logo.png"
+              width="70%"
+              alt="logo"
+              className="center"
+            />
+          </div>
+        </header>
+        <main className="holder" />
+        <div className="letra">
+          <h2>Musica</h2>
+          <h3
+            style={{
+              backgroundColor: "aliceblue",
+            }}
+          >
+            Desde el Synthwave hasta el Dark Synth, dias soleados a oscuras
+            ciudades futuristas. Una playlist creada para todos los gustos
+          </h3>
+        </div>
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/05IllTPaA0ReU2aBwU36JW?utm_source=generator"
+          width="100%"
+          heigth="600%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+    </>
+  );
+};
+
+export default Musica;
